72029da55b228d1dfd60ee0d9ef0f28822aa5295
Małgorzata Jagieła <malgorzata.jagiela@hotmail.com>
2024-12-17 18:22:41 +0100

    Add project files.

---
+++ Stationery/Stationery.cs
@@ -0,0 +1,127 @@
+﻿namespace Stationery;
+
+public abstract class WritingInstrument
+{
+    public abstract bool CanWrite { get; }
+
+    public virtual void Write(string text)
+    {
+        if(CanWrite)
+            Console.WriteLine(text);
+    }
+}
+
+public class EternalLaserMarker : WritingInstrument
+{
+    public override bool CanWrite => true;
+}
+
+public class BallPen : WritingInstrument
+{
+    private bool _isOn;
+
+    public override bool CanWrite => _isOn;
+
+    public void Click() => _isOn = !_isOn;
+
+    public override void Write(string text)
+    {
+        if (!CanWrite)
+            return;
+
+        Console.WriteLine(text);
+    }
+}
+
+public class Pencil : WritingInstrument, ISharpenable
+{
+    private int _charactersWritten;
+    public bool IsSharp { get; private set; }
+
+    public override bool CanWrite => IsSharp;
+
+    public void Sharpen()
+    {
+        IsSharp = true;
+        _charactersWritten = 0;
+    }
+
+    public override void Write(string text)
+    {
+        if (!CanWrite)
+            return;
+
+        Console.WriteLine(text);
+
+        _charactersWritten += text.Length;
+
+        if (_charactersWritten > 30) // You will improve it when you work on your own
+        {
+            IsSharp = false;
+        }
+    }
+}
+
+public class Crayon : WritingInstrument, ISharpenable
+{
+    private int _charactersWritten;
+    private ConsoleColor _color;
+
+    public Crayon(ConsoleColor color) => _color = color;
+
+    public override bool CanWrite => IsSharp;
+    public bool IsSharp { get; private set; }
+
+    public void Sharpen()
+    {
+        IsSharp = true;
+        _charactersWritten = 0;
+    }
+
+    public override void Write(string text)
+    {
+        if (!CanWrite)
+            return;
+
+        Console.ForegroundColor = _color;
+        Console.WriteLine(text);
+        Console.ResetColor();
+
+        _charactersWritten += text.Length;
+
+        if (_charactersWritten > 30) // You will improve it when you work on your own
+        {
+            IsSharp = false;
+        }
+    }
+}
+
+public class StationeryTester
+{
+    public static void Write(string text, IEnumerable<WritingInstrument> instruments)
+    {
+        foreach (var instrument in instruments)
+            instrument.Write(text);
+    }
+}
+
+public interface ISharpenable
+{
+    bool IsSharp { get; }
+
+    void Sharpen();
+}
+
+public static class Sharpener
+{
+    public static void SharpenAll(IEnumerable<ISharpenable> instruments)
+    {
+        foreach (var instrument in instruments)
+        {
+            if (!instrument.IsSharp)
+            {
+                instrument.Sharpen();
+            }
+        }
+    }
+}
---
+++ UserInterface/Program.cs
@@ -0,0 +1,66 @@
+﻿using Stationery;
+
+//var pencil1 = new Pencil();
+//Console.WriteLine(pencil1.IsSharp);
+//pencil1.Sharpen();
+//Console.WriteLine(pencil1.IsSharp);
+
+//pencil1.Write("The pencil can write");
+//Console.WriteLine(pencil1.IsSharp);
+//pencil1.Write("The pencil can still write");
+//Console.WriteLine(pencil1.IsSharp);
+//pencil1.Write("The pencil can still write");
+
+// var ballPen = new BallPen();
+// Console.WriteLine(ballPen.CanWrite);
+// ballPen.Write("I am trying to write with my pen, but it is off");
+// ballPen.Click();
+// Console.WriteLine(ballPen.CanWrite);
+// ballPen.Write("I am trying to write with my pen, and now it is on. Hurray !!");
+
+//var redCrayon = new Crayon(ConsoleColor.Red);
+//Console.WriteLine(redCrayon.IsSharp);
+//redCrayon.Sharpen();
+//Console.WriteLine(redCrayon.IsSharp);
+//redCrayon.Write("Some text");
+
+//var blueCrayon = new Crayon(ConsoleColor.Blue);
+//blueCrayon.Sharpen();
+//blueCrayon.Write("This text will be blue");
+
+////This file needs to compile and work. To achieve this you need to implement necessary changes in the Stationery.cs file in the Utilities project
+
+var redCrayon = new Crayon(ConsoleColor.Red);
+
+Console.WriteLine($"The red crayon is {(redCrayon.IsSharp ? "" : "not ")}sharp");
+redCrayon.Sharpen();
+Console.WriteLine($"The red crayon is {(redCrayon.IsSharp ? "" : "not ")}sharp");
+
+var blueCrayon = new Crayon(ConsoleColor.Blue);
+blueCrayon.Sharpen();
+
+var pencil = new Pencil();
+pencil.Sharpen();
+
+var ballPen = new BallPen();
+ballPen.Click();
+
+var yellowCrayon = new Crayon(ConsoleColor.Yellow);
+yellowCrayon.Sharpen();
+
+var ballPen2 = new BallPen();
+ballPen2.Click();
+
+var writableInstruments = new WritingInstrument[] { redCrayon, blueCrayon, pencil, ballPen, yellowCrayon, ballPen2 };
+
+StationeryTester.Write("Some text", writableInstruments);
+StationeryTester.Write("Some text", writableInstruments);
+StationeryTester.Write("Some text", writableInstruments);
+StationeryTester.Write("Some text", writableInstruments);
+StationeryTester.Write("Some text", writableInstruments);
+
+var laserPointer = new EternalLaserMarker();
+laserPointer.Write("HAHAH This always work");
+
+Sharpener.SharpenAll([redCrayon, blueCrayon, yellowCrayon, pencil ]);
+StationeryTester.Write("Some text", writableInstruments);
